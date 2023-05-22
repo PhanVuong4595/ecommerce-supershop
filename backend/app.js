f1d088de9ec57714361227fb3c7a51dd91a60da9
Phan Vuong <pdvuongtb@gmail.com>
2023-05-22 11:36:08 +0700

    setup - 2

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,43 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 
+// import db
 import connectDatabase from "./config/database";
 
+// import routes
+
+//components
 const app = express();
 dotenv.config();
 
 // middleware
-app.use(cors());
+app.use(
+  cors({
+    //     origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 app.use(morgan("tiny"));
 app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// uploads;
+app.use("/", express.static("uploads"));
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 
 // connect database
 connectDatabase();
+
+//Router
+app.get("/", (req, res) => {
+  res.send("Backend is Running..");
+});
 
 // connection
 app.listen(process.env.PORT, () => {
